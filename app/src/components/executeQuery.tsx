3004255d801dcf47fd41bb7bd17490a9c0736dd6
Yaroslav <yaroslav.shcherban@sisense.com>
2023-09-19 20:31:32 +0300

    executeQuery updated

--- app/src/components/executeQuery.tsx
+++ app/src/components/executeQuery.tsx
@@ -1,59 +1,94 @@
 import React from 'react';
 import { useExecuteQuery } from "@sisense/sdk-ui";
 import * as DM from "./sample-ecommerce";
-import { measures, filters } from "@sisense/sdk-data";
 import { BarChart, CartesianChartDataOptions } from "@sisense/sdk-ui";
-import { DataSource, Attribute, Filter, Measure } from '@sisense/sdk-data';
+import { 
+  measures, 
+  filters,
+  DataSource, 
+  Attribute, 
+  Filter, 
+  Measure, 
+  createDimension, 
+  createAttribute 
+} from '@sisense/sdk-data';
+
+type Dimensions = [{
+  name: string,
+  attrs: Attribute
+}]
 
 export type executeQueryParams = {
-  dataModel?: {
-    DataSource?: string
+  dataModel: {
+    DataSource: DataSource
+    dimensions: Dimensions,
+    dataOptions: CartesianChartDataOptions,
+    
+    measures?: Measures,
+    filters?: Filters,
   },
-  dimensions?: Attribute[],
-  dataOptions?: CartesianChartDataOptions,
   
-  measures?: Measure[],
-  filters?: Filter[],
   chartType?: string
   errorMsg?: string
   loadingMsg?: string,
 }
 
+export type Measures = [{
+  type: string,
+  attrName: Attribute
+}] 
+
+export type Filters = [{
+  type: string,
+  attrName: Attribute,
+  value: number
+}] 
+
+const prepareDimensions = (dimenstions: Dimensions) => dimenstions.map(dimenstion => 
+  createDimension({
+      name: dimenstion.name,
+      [dimenstion.attrs.name]: createAttribute({
+        name: dimenstion.attrs.name,
+        type: dimenstion.attrs.type,
+        expression: dimenstion.attrs.expression,
+      })
+  }))
+
+const prepareMeasures = (measuresArr: Measures) => measuresArr.map(measure => {
+  switch(measure.type) {
+    case 'sum':
+      return measures.sum(measure.attrName);
+    default:
+      return measures.sum(measure.attrName);
+  }
+})
+
+const prepareFilters = (filtersArr: Filters) => filtersArr.map(filter => {
+  switch(filter.type) {
+    case 'greaterThan':
+      return filters.greaterThan(filter.attrName, filter.value);
+    default:
+      return filters.greaterThan(filter.attrName, filter.value);
+  }
+})
 
 export const executeQuery = ({
-  dataModel = DM,
-  dimensions = [DM.Commerce.Gender, DM.Commerce.Quantity, DM.Commerce.Revenue],
-  dataOptions = {
-    "category":[
-       {
-          "name":"Gender",
-          "type":"string"
-       }
-    ],
-    "value":[
-       {
-          "name":"Quantity",
-          "aggregation":"sum"
-       },
-       {
-          "name":"Revenue",
-          "aggregation":"sum"
-       }
-    ],
-    "breakBy":[]
-  },
-  measures = [],
-  filters = [],
+  dataModel,
   chartType,
   errorMsg = 'Error',
   loadingMsg = 'Loading...',
 }: executeQueryParams) => {
-    const { data, isLoading, isError } = useExecuteQuery({
-        dataSource: dataModel.DataSource,
-        dimensions: dimensions,
-        measures: measures,
-        filters: filters,
-      });
+  
+  const queryObject = {
+      dataSource: dataModel.DataSource,
+      measures: (dataModel.measures 
+      && dataModel.measures.length && [...prepareMeasures(dataModel.measures)]) || [],
+      dimensions: [...prepareDimensions(dataModel.dimensions)],
+      filters: (dataModel.filters 
+        && dataModel.filters.length && [...prepareFilters(dataModel.filters)]) || [],
+    }
+
+    const { data, isLoading, isError } = useExecuteQuery(queryObject);
       if (isLoading) {
         return <div>{loadingMsg}</div>;
       }
@@ -61,7 +96,7 @@ export const executeQuery = ({
         return <div>{errorMsg}</div>;
       }
       if (data) {
-        return <BarChart dataSet={data} dataOptions={dataOptions}/>
+        return <BarChart dataSet={data} dataOptions={dataModel.dataOptions}/>
       }
       return null;
 }
