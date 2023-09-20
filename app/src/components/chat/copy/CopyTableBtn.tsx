913ef47f64f5b432f94abde5e2eb210bf4861180
Yaroslav <yaroslav.shcherban@sisense.com>
2023-09-20 17:13:07 +0300

    btn copy table fixes

--- app/src/components/chat/copy/CopyTableBtn.tsx
+++ app/src/components/chat/copy/CopyTableBtn.tsx
@@ -10,12 +10,36 @@ export const CopyTableBtn = ({containerRef}: {containerRef: React.RefObject<HTML
       return;
     }
     
-    const tableHTMLString = containerRef.current.outerHTML;
+    const tableCaptions =  document.querySelectorAll('.fixedDataTableLayout_rowsContainer > .fixedDataTableRowLayout_rowWrapper .fixedDataTableLayout_header .public_fixedDataTableCell_cellContent');
+    const tableData =  document.querySelectorAll('.fixedDataTableLayout_rowsContainer .fixedDataTableRowLayout_rowWrapper > .public_fixedDataTable_bodyRow .public_fixedDataTableCell_cellContent');
+    
+
+    const table = document.createElement("table");
+    const headers = document.createElement("tr");
+    table.appendChild(headers);
+    
+    tableCaptions.forEach(el => {
+        headers.innerHTML +='<th>' + el.textContent + '</th>'
+    })
+
+    let newTr:any;
+    const captionLength = tableCaptions.length;
+    tableData.forEach((v, k) => {
+        if((k + 1) % captionLength) {
+            if(newTr) {
+                newTr.innerHTML +='<td>' + v.textContent + '</td>';
+            }
+        } else {
+            newTr = document.createElement("tr");
+            newTr.innerHTML ='<td>' + v.textContent + '</td>';
+            table.appendChild(newTr);
+        }
+    })
 
     try {
         await navigator.clipboard.write([
             new ClipboardItem({
-                'text/html': new Blob([tableHTMLString], {
+                'text/html': new Blob([table.outerHTML], {
                     type: 'text/html',
                 }),
             }),
